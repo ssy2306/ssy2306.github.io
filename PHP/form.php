29255283f3e97a0683519a4a98138707b80bd353
Shahil Singh Yadav <yshahil0623@gmail.com>
2023-03-27 13:01:25 +0530

    form connect
    connect try

---
+++ PHP/form.php
@@ -0,0 +1,16 @@
+<?php
+//gettig the data from the form
+$email=POST['Email']
+$name=POST['Name']
+$subject = $_POST["Subject"];
+$message = $_POST["Message"];
+$query = $_POST["Query"];
+$to='2028133@gmail.com';
+$txt ="Name = ".$name."\r \n Email= ". $email . "\r \c Subject= ".$subject . "\r \n Message= ". $message . "\r \n Querry= ". $query;
+$headers = "From : https://ssy2306.github.io/HTML/index/homepage.html";
+if($email!=NULL){
+    mail($to, $subject, $txt, $headers);
+}
+//redirected
+header("Location: thankyou.html");
+?>
