425704b01d11863387646f8ed5a908d4bc83a8a0
Shahil Singh Yadav <yshahil0623@gmail.com>
2023-03-30 09:42:03 +0530

    form edit

--- PHP/form.php
+++ PHP/form.php
@@ -6,12 +6,24 @@ $name=test_input($_POST['Name']);
 $subject = test_input($_POST["Subject"]);
 $message = test_input($_POST["Message"]);
 $query = test_input($_POST["Query"]);
+$body=$message;
 $to='2028133@gmail.com';
-$txt ="Name = ".$name."\r \n Email= ". $email . "\r \c Subject= ".$subject . "\r \n Message= ". $message . "\r \n Querry= ". $query;
+$txt ="Name = $name  \n Email=  $email  \n Subject= $subject  \n Message=  $message \n Querry= $query";
 $headers = "From : https://ssy2306.github.io/HTML/index/homepage.html";
 if($email!=NULL){
-    mail($to, $subject, $txt, $headers);
-    header("Location: thankyou.html");
-}
+   if(mail($to, $subject, $body)){
+   echo "Thankyou";
+   } else{
+    echo "Error Occured";
+   }
+} else{
+    echo "Error Occured";
+   }
 //redirected
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
 ?>
