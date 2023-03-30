109c4258cc86a689957423efc535f10d1415df3a
Shahil Singh Yadav <yshahil0623@gmail.com>
2023-03-30 09:23:17 +0530

    color in span

--- PHP/form.php
+++ PHP/form.php
@@ -1,10 +1,11 @@
 <?php
 //gettig the data from the form
-$email=$_POST['Email']
-$name=$_POST['Name']
-$subject = $_POST["Subject"];
-$message = $_POST["Message"];
-$query = $_POST["Query"];
+if($_SERVER["REQUEST_METHOD"]=="POST")
+$email=test_input($_POST['Email']);
+$name=test_input($_POST['Name']);
+$subject = test_input($_POST["Subject"]);
+$message = test_input($_POST["Message"]);
+$query = test_input($_POST["Query"]);
 $to='2028133@gmail.com';
 $txt ="Name = ".$name."\r \n Email= ". $email . "\r \c Subject= ".$subject . "\r \n Message= ". $message . "\r \n Querry= ". $query;
 $headers = "From : https://ssy2306.github.io/HTML/index/homepage.html";
