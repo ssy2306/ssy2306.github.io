a573ddff178331e130dcd2f44a4f90e786da9206
Shahil Singh Yadav <yshahil0623@gmail.com>
2023-04-15 21:31:13 +0530

    cursor animation

--- JS/cursor.js
+++ JS/cursor.js
@@ -4,3 +4,20 @@ document.onmousemove=function(a){
     cursor.style.top=(a.pageY-15)+ "px";
     cursor.style.display="block";
 };
+
+// the name part
+const text = "Shahil Singh Yadav";
+let index = 0;
+
+function typeText() {
+  document.getElementById("typing-text").textContent = text.slice(0, index);
+  index++;
+
+  if (index > text.length) {
+    index = 0;
+  }
+
+  setTimeout(typeText, 300);
+}
+
+typeText();
